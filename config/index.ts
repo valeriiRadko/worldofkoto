3450c83d3b6199f484473f1783a818dec4a09757
Valerii Radko <valerii.radko@gmail.com>
2022-08-04 16:49:51 +0200

    update team config

--- config/index.ts
+++ config/index.ts
@@ -14,39 +14,39 @@ export const AVALANCHE_MAINNET_PARAMS = {
 
 export const team: TeamMember[] = [
   {
-    name: "Jordan Chaney",
+    name: "Sanayei",
     position: "Co-founder/ceo",
     description:
-      "Digital marketer with over 10 years experience leading social media strategy and campaigns within the sport and automotive industries.",
-    photo: "/jordan.jpeg",
+      "Digital marketer with over 10 years experience leading digital strategy for a variety of top brands. Likes to right-click-save and go fast.",
+    photo: "/team/blank.jpeg",
   },
   {
     name: "Lou Manace",
     position: "CTO",
     description:
       "Experienced software engineer who specializes in the development of decentralized applications running on blockchain technology.",
-    photo: "/lou.jpeg",
+    photo: "/team/blank.jpeg",
   },
   {
     name: "Tommy Hines",
     position: "Creative director",
     description:
       "11+ years of experience handcrafting websites, apps, and digital strategies focused on increasing conversion rates and customer experiences.",
-    photo: "/tommy.jpeg",
+    photo: "/team/tommy.jpeg",
   },
   {
     name: "Greg Larson",
     position: "Lead CREATIVE Writer",
     description:
       "Author, ghostwriter, and editor of over 100 books, including USA Today and Wall Street Journal bestsellers.",
-    photo: "/greg.jpeg",
+    photo: "/team/greg.jpeg",
   },
   {
     name: "Sasha Belitskaja",
     position: "3d Artist",
     description:
       "Sasha is an architectural designer and XR developer who’s obsessed with creating new aesthetics. She’s also the co-founder of iHeartBlob, an award-winning architectural design studio.",
-    photo: "/sasha.jpeg",
+    photo: "/team/sasha.jpeg",
   },
 ];
 
