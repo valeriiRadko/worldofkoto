d5434ded8d760e32635da7789bbe4f94cf0d46b9
Valerii Radko <valerii.radko@gmail.com>
2022-08-05 11:07:43 +0200

    extend footer with power by section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,32 +1,54 @@
+import Image from "next/image";
 import React from "react";
 
 import styles from "./Footer.module.scss";
 
 function Footer() {
   return (
-    <footer className={styles.footer}>
-      <div className={styles["terms-and-conditions"]}>
-        ®Copyright {new Date().getFullYear()}. The Metaversed Studio Ltd. All
-        rights reserved.
+    <footer className={styles.root}>
+      <div className={styles["powered-by-container"]}>
+        <span className={styles["powered-by"]}>Powered by</span>
+        <span className={styles["image-wrapper"]}>
+          <Image
+            src="/images/powered-by/cubiq.png"
+            alt="cubiq"
+            width={178}
+            height={87}
+          />
+        </span>
+        <span className={styles["image-wrapper"]}>
+          <Image
+            src="/images/powered-by/avalanche.png"
+            alt="avalanche"
+            width={70}
+            height={78}
+          />
+        </span>
       </div>
-      <div className={styles.links}>
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.link}
-        >
-          Privacy
-        </a>
-        •
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.link}
-        >
-          Terms & Conditions
-        </a>
+      <div className={styles.footer}>
+        <div className={styles["terms-and-conditions"]}>
+          ®Copyright {new Date().getFullYear()}. The Metaversed Studio Ltd. All
+          rights reserved.
+        </div>
+        <div className={styles.links}>
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Privacy
+          </a>
+          •
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Terms & Conditions
+          </a>
+        </div>
       </div>
     </footer>
   );
--- config/index.ts
+++ config/index.ts
@@ -18,35 +18,35 @@ export const team: TeamMember[] = [
     position: "Co-founder/ceo",
     description:
       "Digital marketer with over 10 years experience leading digital strategy for a variety of top brands. Likes to right-click-save and go fast.",
-    photo: "/team/blank.jpeg",
+    photo: "/images/team/blank.jpeg",
   },
   {
     name: "Lou Manace",
     position: "CTO",
     description:
       "Experienced software engineer who specializes in the development of decentralized applications running on blockchain technology.",
-    photo: "/team/blank.jpeg",
+    photo: "/images/team/blank.jpeg",
   },
   {
     name: "Tommy Hines",
     position: "Creative director",
     description:
       "11+ years of experience handcrafting websites, apps, and digital strategies focused on increasing conversion rates and customer experiences.",
-    photo: "/team/tommy.jpeg",
+    photo: "/images/team/tommy.jpeg",
   },
   {
     name: "Greg Larson",
     position: "Lead CREATIVE Writer",
     description:
       "Author, ghostwriter, and editor of over 100 books, including USA Today and Wall Street Journal bestsellers.",
-    photo: "/team/greg.jpeg",
+    photo: "/images/team/greg.jpeg",
   },
   {
     name: "Sasha Belitskaja",
     position: "3d Artist",
     description:
       "Sasha is an architectural designer and XR developer who’s obsessed with creating new aesthetics. She’s also the co-founder of iHeartBlob, an award-winning architectural design studio.",
-    photo: "/team/sasha.jpeg",
+    photo: "/images/team/sasha.jpeg",
   },
 ];
 
